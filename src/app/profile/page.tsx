58e6d367b2335755458ad58c705afc017f3893ad
DinisSkizee <dinissilvauk@gmail.com>
2023-09-29 18:01:23 +0100

    lil fix

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,21 +1,21 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import ProfilePage from "./profile-server";
 import { useGetCustomerQuery } from "@gql/schema";
 
 export default function ProfileClient() {
   const { user } = useUser();
-  const customerAccessToken = localStorage.getItem("customer_access_token");
+  const customerAccessToken =
+    localStorage.getItem("customer_access_token") ?? "";
   const { data, error } = useGetCustomerQuery({
     variables: {
-      customerAccessToken: customerAccessToken ?? "",
+      customerAccessToken: customerAccessToken,
     },
   });
-  useEffect(() => {
-    console.log("data", data);
-    console.log("error", error);
-  }, [data, error]);
-  console.log(user);
+
+  console.log("data", data);
+  console.log("error", error);
+
   return <ProfilePage username={user?.name} image={user?.picture} />;
 }
